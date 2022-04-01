88ba35043be008b31aa186cafcf83620e4009061
Max Zhao <max.zhao@arsbrevis.de>
2022-04-01 18:38:48 +0200

    Updated merge script

--- scripts/merge_results_rep.sh
+++ scripts/merge_results_rep.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [ -z $2 ]; then
+	echo "$0 [dataset prefix] [destination csv]"
+	exit
+fi
+
 EXPORTSCRIPT=./scripts/extract.py
-MERGED_OUTPUT=output.csv
+MERGED_OUTPUT=$2
 
 if [ -f $MERGED_OUTPUT ]; then
 	echo "${MERGED_OUTPUT} already exists."
@@ -10,6 +15,11 @@ fi
 
 for repdir in $1*; do
 	repnum=${repdir##*_}
-	$EXPORTSCRIPT --move_failed 1 "$repdir" "${repnum}.csv"
-	cat "${repnum}.csv" >> $MERGED_OUTPUT
+	# $EXPORTSCRIPT --move_failed 1 "$repdir" "/tmp/${repnum}.csv"
+	$EXPORTSCRIPT "$repdir" "/tmp/${repnum}.csv"
+	if [ $repnum = "rep1" ]; then
+		cat "/tmp/${repnum}.csv" >> $MERGED_OUTPUT
+	else
+		tail -n+1 "/tmp/${repnum}.csv" >> $MERGED_OUTPUT
+	fi
 done
