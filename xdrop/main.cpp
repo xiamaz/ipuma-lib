1ef4da94ca1a9213eb097c7861d3db8aa7ce1f1d
Luk Burchard <luk.burchard@gmail.com>
2022-09-30 23:29:07 -0700

    WIP calc L and U

--- xdrop/main.cpp
+++ xdrop/main.cpp
@@ -101,30 +101,35 @@ int main(int argc, char** argv) {
   // 3 [0,0,1,3,2,1,0,0,1]
   // 4 [0,0,0,2,4,3,2,1,0]
   // m [0,1,1,1,3,5,4,3,2]
-  const std::string query{"AATGAGAA"};
-  const std::string reference{"AATGA"};
-  int X = 1;
-  int neginf = -9;
+  // const std::string query{"AATGAGAA"};
+  // const std::string reference{"AATGA"};
+  const std::string query = queries[5];
+  const std::string reference = refs[5];
+
+
+
+
+  int X = 4;
+  int neginf = -99;
   int T_prime = 0, T = 0, L = 0, U = 0; 
 
   int m = reference.length() + 1;
   int n = query.length() + 1;
-  // Matrix<int> H(m, n, 0);
-  // Matrix<int> C(15, 15, 0);
+  Matrix<int> H(m, n, 0);
+  Matrix<int> C(m, n, 0);
 
   int * k1 = &((int*) calloc(min(m, n) + 1, sizeof(int)))[1];
   int * k2 = &((int*) calloc(min(m, n) + 1, sizeof(int)))[1];
   int * k3 = &((int*) calloc(min(m, n) + 1, sizeof(int)))[1];
 
-  auto cell_update = [&](int i, int j, int*k1, int*k2, int*k3, int z){
+  auto cell_update = [&](int i, int j, int* k1, int* k2, int* k3, int z){
     auto [index, score] = maxtuple({
-      0,
       k2[z] - GAP_PENALTY,
       k2[z-1] - GAP_PENALTY,
       k1[z-1] + simpleSimilarity(reference[i-1], query[j-1])
     });
     if (score < T - X) {
-      PLOGW.printf("(%d, %d) DROP", i, j);
+      // PLOGW.printf("(%d, %d) DROP", i, j);
       score = neginf;
     }
     k3[z] = score;
@@ -147,16 +152,48 @@ int main(int argc, char** argv) {
   int c = 0;
   do {
     k = k + 1;
+    int lastU = 0;
     for (size_t i = L; i < U+1; i++) {
       auto j = k - i - 1;
       int _j = j + 1;
       int _i = i + 1;
       int score = cell_update(_i, _j, k1, k2, k3, i);
-      // C(k-1, i) = ++c;
+      if (score == neginf) {
+        if ((k3[i-1] == neginf && i != 0) || i == 0) {
+          L = i;
+        }
+        if (k3[i-1] != neginf) {
+          lastU = i;
+        }
+      }
+      C(_i, _j) = 1;
       T_prime = max(T_prime, score);
     }
-    L = 0;
-    U = U+1;
+    // find first non neg score
+    // L = ;
+    if (k3[U] == neginf) {
+      U = lastU+1;
+    }
+    PLOGE.printf("L(%d), U(%d), lastU(%d), k3[L](%d), k3[U](%d)", L, U, lastU, k3[L], k3[U]);
+
+    // int minL = max(M, N);
+    // for (int i = U; i >= 0; i--) {
+    //   int s = H(i, k-i);
+    //   if (s > neginf) {
+    //     minL = i;
+    //   }
+    // }
+
+    // int maxU = 0;
+    // for (int i = 0; i < U+1; i++) {
+    //   int s = H(i, k-i);
+    //   if (s > neginf) {
+    //     maxU = i;
+    //   }
+    // }
+    PLOGW.printf("L(%d), U(%d)",  L, U);
+    L = minL;
+    U = maxU;
     L = max(L, k+1-N);
     U = min(U, M-1);
     T = T_prime;
@@ -164,7 +201,7 @@ int main(int argc, char** argv) {
   } while  (L <= U+1);
 
   PLOGI << H.toString();
-  // PLOGI << C.toString();
+  PLOGI << C.toString();
 
   return 0;
 }
