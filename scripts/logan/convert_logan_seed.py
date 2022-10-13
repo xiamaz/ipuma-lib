9d6f8527a34c68fadb1684975d806563783783c8
Luk Burchard <luk.burchard@gmail.com>
2022-10-13 21:37:23 +0200

    Coversion script

--- scripts/logan/convert_logan_seed.py
+++ scripts/logan/convert_logan_seed.py
@@ -2,36 +2,82 @@
 
 from pathlib import Path
 import re
+import numpy as np
 
 pattern = re.compile(r"^(?P<seq1>\w+)\W+(?P<seed1>\w+)\W+(?P<seq2>\w+)\W+(?P<seed2>\w+)\W+(?P<strand>\w+)$")
-lines = Path("./example.txt").read_text().splitlines()
 As = []
 Bs = []
-for l in lines:
-        p = pattern.search(l)
-        g = p.groupdict()
-        if g["strand"] == "n":
-                As.append(g["seq1"][int(g["seed1"]):])
-                Bs.append(g["seq2"][int(g["seed2"]):])
-                As.append(g["seq1"][:int(g["seed1"])][::-1])
-                Bs.append(g["seq2"][:int(g["seed2"])][::-1])
 
 
+# Good logan
+# for l in lines:
+#         p = pattern.search(l)
+#         g = p.groupdict()
+#         seq1 = g["seq1"]
+#         seq2 = g["seq2"]
+#         seed1 = int(g["seed1"])
+#         seed2 = int(g["seed2"])
+#         if g["strand"] == "n":
+#                 As.append(g["seq1"][int(g["seed1"]):])
+#                 Bs.append(g["seq2"][int(g["seed2"]):])
+#                 As.append(g["seq1"][:int(g["seed1"])][::-1])
+#                 Bs.append(g["seq2"][:int(g["seed2"])][::-1])
+#         else:
+#                 A = seq1
+#                 B = "".join(map(lambda x: {"A":"T", "T":"A", "G": "C", "C": "G"}[x], g["seq2"]))[::-1]
+#                 k = 17
+#                 _seed2 = len(B)-seed2-k
+#                 As.append(A[seed1:])
+#                 Bs.append(B[_seed2:])
+#                 As.append(A[:seed1][::-1])
+#                 Bs.append(B[:_seed2][::-1])
 
-#%%
-with open("As.txt", "w") as fd:
-                fd.write('\n'.join(As) + '\n')
-with open("Bs.txt", "w") as fd:
-                fd.write('\n'.join(Bs) + '\n')
+# Bad logan
+c = 0
+with open("elba.ipu.test.result.ipuinputmatrix.mm", "r") as lines:
+        for l in lines:
+                l.strip("\n")
+                p = l.split("\t")
+                # g = p.groupdict()
+                if len(p) != 6:
+                        continue
+                seq1 = p[2]
+                seq2 = p[2+2]
+                seed1 = int(p[2+1])
+                seed2 = int(p[2+2+1])
 
-#%%
-# import matplotlib.pyplot as plt
-# import numpy as np
+                if len(seq1) > len(seq2):
+                        seq1, seq2 = seq2, seq1
+                        seed1, seed2 = seed2, seed1 
+
+                if len(seq1) < 10 or len(seq2) < 10:
+                        c+=1
+                        continue
+                if len(seq1[seed1:]) > 10 and len(seq2[seed2:]) > 10:
+                        As.append(seq1[seed1:])
+                        Bs.append(seq2[seed2:])
+                if len(seq1[:seed1]) > 10 and len(seq2[:seed2]) > 10:
+                        As.append(seq1[:seed1][::-1])
+                        Bs.append(seq2[:seed2][::-1])
+print(f"Skipped {c} lines")
+                
+
 # la = np.asarray([len(a) for a in As])
 # lb = np.asarray([len(b) for b in Bs])
-
 # for x, y in zip(la, lb):
 #         print(f"c({x:5}, {y:5}):> weight {x*y}")
 
-# # plt.hist(la * lb)
+
+#%%
+# import matplotlib.pyplot as plt
+
+# plt.hist(la * lb)
+# plt.hist(lb)
 # plt.hist(la)
+
+#%%
+
+with open("elbaAs.txt", "w") as fd:
+        fd.write("\n".join(As))
+with open("elbaBs.txt", "w") as fd:
+        fd.write("\n".join(Bs))
