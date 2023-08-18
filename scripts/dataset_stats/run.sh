351a398eb8519afd146ed6bb4acd21d9210893a1
Max Zhao <max.zhao@arsbrevis.de>
2023-08-18 18:08:31 +0200

    Uncommenting script

--- scripts/dataset_stats/run.sh
+++ scripts/dataset_stats/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# DSNAME=simulated85
-# python dataset_stats.py ../data/cmps_${DSNAME}_multi.json.gz ../data/seqs_${DSNAME}_multi.json.gz > dataset_stats/${DSNAME}_stats.json
+DSNAME=simulated85
+python dataset_stats.py ../data/cmps_${DSNAME}_multi.json.gz ../data/seqs_${DSNAME}_multi.json.gz > dataset_stats/${DSNAME}_stats.json
 
 DSNAME=ecoli
 python dataset_stats.py ../data/cmps_${DSNAME}_multi_single.json.gz ../data/seqs_${DSNAME}_multi_single.json.gz > dataset_stats/${DSNAME}_stats.json
